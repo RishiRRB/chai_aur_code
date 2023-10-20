c7ea18ba8a9c5f30dd2e2da49338c40bd1d35d40
RishiRRB <148525785+RishiRRB@users.noreply.github.com>
2023-10-20 11:14:53 +0000

    conversion

--- basics01/01_Variable.js
+++ basics01/01_Variable.js
@@ -2,7 +2,7 @@ const accountId = 12324;
 let userName = "Rishi"
 var userPassword = "122343";
 userCity = 'Ahmedabad';// This is not a proper way to assign value to the varible.
-let userState;
+let userState;// the value of this variable is undefined;
 
 console.log(accountId)
 
