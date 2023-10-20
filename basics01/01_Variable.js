2c10bb56aa6e8f45f06f87adf81d12d5ce4fa56e
RishiRRB <148525785+RishiRRB@users.noreply.github.com>
2023-10-20 10:32:34 +0000

    variable

---
+++ basics01/01_Variable.js
@@ -0,0 +1,9 @@
+const accountId = 12324;
+let userName = "Rishi"
+var userPassword = "122343";
+userCity = 'Ahmedabad';// This is not a proper way to assign value to the varible.
+let userState;
+
+console.log(accountId)
+
+console.table([accountId,userName,userPassword,userCity,userState])
